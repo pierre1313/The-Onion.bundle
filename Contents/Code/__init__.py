84d14f44ea904d99bf543fc4d375ec8cf46e7370
pierre1313 <p.dellanave@gmail.com>
2010-09-14 14:59:15 +0800

    Fixed functionality + v0 ->v1

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,29 +1,45 @@
-from PMS import Plugin, Log, XML, HTTP, JSON, Prefs, RSS, Utils
-from PMS.MediaXML import *
-from PMS.Shorthand import _L, _R, _E, _D
+from PMS import *
+from PMS.Objects import *
+from PMS.Shortcuts import *
+import re
 
 TO_PLUGIN_PREFIX   = "/video/theonion"
-TO_BASE_URL        = "http://www.theonion.com/"
+TO_BASE_URL        = "http://www.theonion.com"
 #TO_SEARCH_URL      = HULU_BASE_URL + "search/"
 
 CACHE_INTERVAL       = 1000 # HTTP cache interval in seconds
 MAX_RESULTS          = "100"
 
-TO_AJAX              = "http://www.theonion.com/content/ajax/onn/list/%s/1/%s" # % max_results % show name
+TO_AJAX              = "http://www.theonion.com/ajax/onn_playlist/%s/%s" #% show name %pagenum
+
+ART = "art-default.jpg"
+NAME = "The Onion"
+ICON = "icon-default.jpg"
 
 ####################################################################################################
 def Start():
-  Plugin.AddRequestHandler(TO_PLUGIN_PREFIX, HandleRequest, "The Onion", "icon-default.jpg", "art-default.jpg")
-  Plugin.AddViewGroup("InfoList", viewMode="InfoList", contentType="items")  
-  Plugin.AddViewGroup("List", viewMode="List", contentType="items")
+  Plugin.AddPrefixHandler(TO_PLUGIN_PREFIX, MainMenu, NAME, ICON, ART)
+  Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")  
+  Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
+  MediaContainer.art = R(ART)
+  MediaContainer.title1 = NAME
+  MediaContainer.viewGroup = "List"
+
+  # Default icons for DirectoryItem
+  DirectoryItem.thumb = R(ICON)
+  
 ####################################################################################################
-def populateFromHTML(show_id, dir, title2=""):
-  dir.SetViewGroup("InfoList")
-  for e in XML.ElementFromString(HTTP.GetCached(TO_AJAX % (MAX_RESULTS, show_id), CACHE_INTERVAL), True).xpath("//li"):
+def populateFromHTML(sender, show_id, show_title = '', replaceParent = False,  page=1):
+
+  dir = MediaContainer(content = 'Items', viewGroup="List", replaceParent = replaceParent, title2=str(show_title) + ' - Page '+str(page))
+  
+  if page>1 :
+    dir.Append(Function(DirectoryItem(populateFromHTML, title = 'Previous Page ...'),show_id = show_id, page = page-1, replaceParent = True))
+  
+  for e in XML.ElementFromURL(TO_AJAX % (show_id,page), CACHE_INTERVAL).xpath("//li"):
     id = TO_BASE_URL + e.xpath("div[@class='image_wrapper']//a")[0].get("href")
     thumb = e.xpath("div[@class='image_wrapper']//a/img")[0].get("src")
-    title = e.xpath("div[@class='image_wrapper']//img")[0].get("title")
-    desc = e.xpath("p/a")[0].text
+    title = e.xpath("p/a")[0].text
     try:
       duration = e.xpath("p/span[@class='date']")[0].text.replace("(","").replace(")","")
       (mins, secs) = duration.split(":")
@@ -32,52 +48,48 @@ def populateFromHTML(show_id, dir, title2=""):
       duration = str(((mins * 60) + int(secs)) * 1000)
     except:
       duration = ""
-    dir.AppendItem(WebVideoItem(id, title, desc, duration, thumb))
+    html = HTTP.Request(id)  
+    link_pattern = re.compile('var video_url = "([^"]+)"')
+    link = link_pattern.search(html)
+    if link != None:
+      link = link.group(1)
+    dir.Append(VideoItem(link, title,'',summary ='', duration = duration, thumb=thumb))
+  dir.Append(Function(DirectoryItem(populateFromHTML, title = 'Next Page ...'),show_id = show_id, show_title = show_title, page = page+1, replaceParent = True))
   return dir
     
-def HandleRequest(pathNouns, count):
-  i=0
-  for x in pathNouns:
-    Log.Add(str(i) + ": " + x)
-    i+=1
-    
-  try:
-    (pathNouns[count-1], title2) = pathNouns[count-1].split("||")
-  except:
-    title2 = ""
-  
-  dir = MediaContainer("art-default.jpg", viewGroup="List", title1="The Onion", title2=title2)  
-  if count == 0:
-    for e in XML.ElementFromString(HTTP.GetCached("http://www.theonion.com/content/video", CACHE_INTERVAL), True).xpath('//ul[@id="categories"]/li'):  
-      id = e.get("id")
-      cls = e.get("class")
-      if cls != "category":
-        title = e.xpath("a")[0].text
-        dir.AppendItem(DirectoryItem("videolist^" + id + "||" + _L(title), _L(title), ""))
-      else:
-        dir.AppendItem(DirectoryItem(e.text + "||" + _L(e.text), _L(e.text), ""))
-        break
-      
-  elif pathNouns[-1].startswith("Shows"):
-    foundShow=0
-    for e in XML.ElementFromString(HTTP.GetCached("http://www.theonion.com/content/video", CACHE_INTERVAL), True).xpath('//ul[@id="categories"]/li'): 
-      id = e.get("id")
-      cls = e.get("class")
-      if foundShow == 0:
-        if cls == "category":
-          foundShow = 1
-      else:
-        try:
-          title = e.xpath("a")[0].get("rel")
-        except:
-          title = e.xpath("span/a")[0].get("rel")
-        show_id = title.replace("-","").replace(" ","")
-        if show_id == "Election08":
-          show_id = "election"
-        dir.AppendItem(DirectoryItem("videolist^" + show_id + "||" + _L(title), _L(title), ""))
-        
-  elif pathNouns[-1].startswith("videolist"):
-    show_id = pathNouns[-1].split("^")[1].lower()
-    dir = populateFromHTML(show_id, dir)
+def MainMenu():
+   dir = MediaContainer("art-default.jpg", viewGroup="List", title2='')  
 
-  return dir.ToXML()
+   categories = XML.ElementFromURL("http://www.theonion.com/content/video", cacheTime=CACHE_INTERVAL, isHTML=True).xpath('//ul[@id="categories"]/li')
+   for e in categories:
+     id = e.get("id")
+     cls = e.get("class")
+     if cls == "category":   
+       dir.Append(Function(DirectoryItem(Shows,title = "Shows")))
+       break
+     else:
+       title = e.xpath("a")[0].get("rel")
+       dir.Append(Function(DirectoryItem(populateFromHTML,title = title),show_id = id, show_title = title))
+   return dir
+   
+def Shows(sender):
+   dir = MediaContainer("art-default.jpg", viewGroup="List", title2='Shows')  
+
+   shows = XML.ElementFromURL("http://www.theonion.com/content/video", cacheTime=CACHE_INTERVAL, isHTML=True).xpath('//ul[@id="categories"]/li')
+   foundshow = 0
+   for e in shows:
+     id = e.get("id")
+     cls = e.get("class")
+     if cls == "category":
+       foundshow = 1
+     else:   
+       if foundshow == 1:
+         try:
+           title = e.xpath("a")[0].get("rel")
+         except:
+           title = e.xpath("span/a")[0].get("rel")
+         dir.Append(Function(DirectoryItem(populateFromHTML,title = title),show_id = id, show_title = title))
+   return dir
+
+def PlayVideo(sender,videofile):
+  return Redirect(videofile)
